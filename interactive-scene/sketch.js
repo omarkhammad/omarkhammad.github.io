3ab618b4ce66a4a064bcb9b82cc52ec485e4b9a1
omarkhammad <omar_hammad_512@yahoo.com>
2024-09-20 14:03:04 -0600

    Update sketch.js

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,13 +1,50 @@
 // Interactive Scene
 // Demo
 
+let playerX;
+let playerY;
+let playerSize = 50;
+let speed;
+let dashCooldown = 240;
+let dashSpeed = 20;
+let dashLength = 30;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  playerX = width / 2;
+  playerY = height / 2;
+  fill("black")
 }
 
 function draw() {
   background(220);
+  if (dashCooldown > 0) {
+    dashCooldown--;
+    speed = 10;
+  } else if (dashCooldown < 0) {
+    if (dashCooldown < -1) {
+      dashCooldown = 240;
+    } else {
+      dashCooldown++;
+    }
+  }
 
-  circle(mouseX, mouseY, 30);
+  if (keyIsDown(32) && dashCooldown === 0); {
+    speed = dashSpeed;
+    dashCooldown = -dashLength;
+  }
+
+
+  if (keyIsDown(87)) { //W
+    playerY -= speed;
+  } else if (keyIsDown(83)) { //S
+    playerY += speed;
+  }
+  if (keyIsDown(65)) { //A
+    playerX -= speed;
+  } else if (keyIsDown(68)) { //D
+    playerX += speed;
+  }
+
+  circle(playerX, playerY, playerSize);
 }
