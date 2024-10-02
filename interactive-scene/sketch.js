50ba0e327e984b54716baae7c6cf0fe0b6a37d3d
omarkhammad <omar_hammad_512@yahoo.com>
2024-10-01 23:59:16 -0600

    Update sketch.js

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,5 +1,4 @@
-// Interactive Scene
-// Demo
+// Omar Hammad's Interactive Scene
 
 let playerX;
 let playerY;
@@ -26,14 +25,17 @@ let dashLength = 8;
 let barHeight = 60;
 let barPadding = 10;
 let dashBarWidthScale = 3;
-let healthBarWidth = 240
-let healthBarTilt = 24
+let healthBarWidth = 240;
+let healthBarTilt = 24;
 
 let rows;
 let columns;
 let minRows = 3;
-let maxRows = 3;
-let totalLasers = 30;
+let midRows = 7;
+let maxRows = 14;
+let minColumns = 10;
+let midColumns = 20;
+let maxColumns = 30;
 let laserSize = 30;
 let laserColor;
 let lowLaserOpacity = 128;
@@ -43,19 +45,42 @@ let laserTimer = laserCooldownTime;
 let laserMode = 0;
 let laserWarningTime = 80;
 let laserOnTime = 120;
+let laserTimeLoss = 5;
+let laserRound = 0;
+let minCooldown = 15;
 
 let titleText = 'L A S E R    D A S H';
 let titleXShitf = 6;
 let titleYShift = 0;
-let colorGradients = []
+let textColorGradients;
+
+
+function gameOver() {
+  background("white");
+  textSize(200);
+
+  // Displays the Game Over text and stops the game
+  for (let textNumber = 0; textNumber < textColorGradients.length; textNumber++) {
+    // Creates multiple layers of text to make a gradient illusion
+    fill(textColorGradients[textNumber]);
+    text("Game Over", width / 2 + titleXShitf * (textColorGradients.length - textNumber), height / 2 + titleYShift * (textColorGradients.length - textNumber));
+  }
+
+  // Displays instructions to restart
+  fill("black");
+  textSize(40);
+  text("press F5 to restart", width / 2, height * 3 / 4);
+}
 
 
 function windowResized() {
+  // Resizes the game if the window size changes
   resizeCanvas(windowWidth, windowHeight);
 }
 
 
 function playerDash() {
+  // Cooldown for player dash
   if (dashTimer > 0) {
     dashTimer--;
   }
@@ -67,6 +92,7 @@ function playerDash() {
     dashTimer++;
   }
 
+  // Allows the player to dash using the mouse button
   if (mouseIsPressed && dashTimer === 0) {
     dash = dashMultiplier;
     dashTimer = -dashLength;
@@ -75,47 +101,51 @@ function playerDash() {
 
 
 function playerMove() {
+  // Moves player up
   if (keyIsDown(87)) { //W
     dy -= acc;
     if (dy < -maxSpeed) {
       dy = -maxSpeed;
     }
-  }
+  } // Moves player down
   else if (keyIsDown(83)) { //S
     dy += acc;
     if (dy > maxSpeed) {
       dy = maxSpeed;
     }
-  }
+  } // Decelerates the player vertically
   else if (Math.abs(dy) > minSpeed) {
     dy = dy * decc;
-  }
+  } // Stops the player vertically
   else {
     dy = 0;
   }
 
+  // Moves the player left
   if (keyIsDown(65)) { //A
     dx -= acc;
     if (dx < -maxSpeed) {
       dx = -maxSpeed;
     }
-  }
+  } // Moves the player right
   else if (keyIsDown(68)) { //D
     dx += acc;
     if (dx > maxSpeed) {
       dx = maxSpeed;
     }
-  }
+  } // Decelerates the player horizontally
   else if (Math.abs(dx) > minSpeed) {
     dx = dx * decc;
-  }
+  } // Stops the player horizontally
   else {
     dx = 0;
   }
 
+  // Calculates the player's total motion
   playerX += dx * dash / (playerSquish / playerMaxSquish / 2 + 1);
   playerY += dy * dash * (playerSquish / playerMaxSquish / 2 + 1);
 
+  // Checks if the player is against the wall
   if (playerX < playerSize) {
     playerX = playerSize;
   }
@@ -132,12 +162,13 @@ function playerMove() {
 
 
 function mouseWheel(event) {
+  // If the player scrolls up, the "squish" variable goes up, making the ball longer 
   if (event.delta < 0) {
     playerSquish += playerSquishSpeed;
     if (playerSquish > playerMaxSquish) {
       playerSquish = playerMaxSquish;
     }
-  }
+  } // If the player scrolls down, the "squish" variable goes down, making the ball flatter 
   else {
     playerSquish -= playerSquishSpeed;
     if (playerSquish < -playerMaxSquish) {
@@ -148,27 +179,44 @@ function mouseWheel(event) {
 
 
 function drawCircle() {
+  // Draws the ball
   fill("green");
   ellipse(playerX, playerY, playerSize * 2 - playerSquish, playerSize * 2 + playerSquish);
 }
 
 
 function lasers() {
+  // Ticks the laser timer
   laserTimer--;
+
+  // If the laser timer is zero, selects a random number of rows and columns and makes them apear translucent
   if (!laserTimer) {
-    //rows = random(minRows, maxRows);
-    //columns = totalLasers - rows;
-    rows = 7; //14;
-    columns = 16; //32; //20;
+    rows = random(minRows, maxRows);
+
+    if (rows > midRows) {
+      columns = random(minColumns, midColumns)
+    } else {
+      columns = random(midColumns, maxColumns)
+    }
+
     laserMode = 1;
     laserColor.setAlpha(lowLaserOpacity);
     playerIsHurt = false;
   }
+  // When the laser is on for long enough, turns off lasers and resets laser timer
   else if (laserTimer < -laserOnTime) {
-    laserTimer = laserCooldownTime;
-    laserMode = 0;
 
+    // Slowly makes the game go faster
+    laserRound++;
+    if (laserCooldownTime - laserTimeLoss * laserRound > minCooldown) {
+      laserTimer = laserCooldownTime - laserTimeLoss * laserRound;
+    } else{
+      laserTimer = minCooldown
+    }
+    
+    laserMode = 0;
   }
+  // When it is time for the laser to be on, it makes the laser opaque and lets them deal damage to the player
   else if (laserTimer < -laserWarningTime) {
     laserColor.setAlpha(highLaserOpacity);
     laserMode = 2;
@@ -177,17 +225,22 @@ function lasers() {
 
   if (laserMode) {
     fill(laserColor);
+    // For loop that creates all the row lasers 
     for (let row = 0; row < rows; row++) {
+      // Creates the rectangle of the row laser
       rect(0, playerSize + barHeight + row * (height - barHeight - playerSize * 2 - laserSize) / (rows - 1), width, laserSize);
+      // If it is time for the laser to deal damage and the the player is touching the row laser, it will decrease the player's health
       if (!playerIsHurt && laserMode === 2 && playerY - playerSize - playerSquish / 2 < laserSize + playerSize + barHeight + row * (height - barHeight - playerSize * 2 - laserSize) / (rows - 1) && playerY + playerSize + playerSquish / 2 > playerSize + barHeight + row * (height - barHeight - playerSize * 2 - laserSize) / (rows - 1)) {
         playerIsHurt = true;
         playerHealth--;
       }
     }
-    console.log(rows);
 
+    // For loop that creates all the column lasers 
     for (let column = 0; column < columns; column++) {
+      // Creates the rectangle of the column laser
       rect(playerSize + column * (width - playerSize * 2 - laserSize) / (columns - 1), barHeight, laserSize, height - barHeight);
+      // If it is time for the laser to deal damage and the the player is touching the column laser, it will decrease the player's health
       if (!playerIsHurt && laserMode === 2 && playerX - playerSize + playerSquish / 2 < laserSize + playerSize + column * (width - playerSize * 2 - laserSize) / (columns - 1) && playerX + playerSize - playerSquish / 2 > playerSize + column * (width - playerSize * 2 - laserSize) / (columns - 1)) {
         playerIsHurt = true;
         playerHealth--;
@@ -198,15 +251,18 @@ function lasers() {
 
 
 function topBar() {
+  // Creates the top black bar
   fill("black");
   rect(0, 0, width, barHeight);
 
+  // Creates the dashing bar
   stroke("white");
   fill("grey");
   rect(barPadding, barPadding, dashCooldown * dashBarWidthScale + playerSize * 2, barHeight - barPadding * 2, barHeight);
   fill(color(78, 79, 255));
   rect(barPadding, barPadding, (dashCooldown - dashTimer) * dashBarWidthScale * (dashTimer >= 0) + playerSize * 2, barHeight - barPadding * 2, barHeight);
 
+  // Creates the red health bar
   fill("grey");
   quad(width - barPadding - healthBarWidth - healthBarTilt, barPadding, width - barPadding - healthBarWidth, barHeight - barPadding, width - barPadding, barHeight - barPadding, width - barPadding - healthBarTilt, barPadding);
   fill("red");
@@ -216,24 +272,20 @@ function topBar() {
 
 
 function title() {
+  // Creates the top title
   textStyle(BOLDITALIC);
   textAlign(CENTER, CENTER);
   textSize(50);
-  fill(255,193,0)
-  text(titleText, width / 2 + titleXShitf * 4, barHeight / 2 + titleYShift * 4);
-  fill(255,154,0)
-  text(titleText, width / 2 + titleXShitf * 3, barHeight / 2 + titleYShift * 3);
-  fill(255,116,0)
-  text(titleText, width / 2 + titleXShitf * 2, barHeight / 2 + titleYShift * 2);
-  fill(255,77,0)
-  text(titleText, width / 2 + titleXShitf, barHeight / 2 + titleYShift);
-  fill(255,0,0)
-  text(titleText, width / 2, barHeight / 2);
+  // Creates multiple layers of text to make a gradient illusion
+  for (let textNumber = 0; textNumber < textColorGradients.length; textNumber++) {
+    fill(textColorGradients[textNumber])
+    text(titleText, width / 2 + titleXShitf * (textColorGradients.length - textNumber), barHeight / 2 + titleYShift * (textColorGradients.length - textNumber));
+  }
 }
 
 
-
 function drawBackground() {
+  // Creates a white curved rectangle as the background to make the curved corners
   background("black");
   fill("white");
   rect(0, barHeight, width, height - barHeight, playerSize);
@@ -241,28 +293,46 @@ function drawBackground() {
 
 
 function setup() {
+  // Makes the game take up the full screen
   createCanvas(windowWidth, windowHeight);
   strokeWeight(3);
 
+  // Sets the player to the center of the screen
   playerX = width / 2;
   playerY = height / 2;
 
+  // Sets the laser's base color
   laserColor = color(230, 0, 0);
+  // Sets the Gradient for the titles
+  textColorGradients = [color(255, 193, 0), color(255, 154, 0), color(255, 116, 0), color(255, 77, 0), color(255, 0, 0)]
 }
 
 
 function draw() {
-  drawBackground();
+  // Only runs the Game Over function if the player health is zero
+  if (!playerHealth) {
+    gameOver();
+  }
+  else {
+    // Draws the white background
+    drawBackground();
 
-  drawCircle();
+    // Draws the player's circle
+    drawCircle();
 
-  topBar();
+    // Draws the top bar, dashing bar, and the health bar
+    topBar();
 
-  title();
+    // Draws the title
+    title();
 
-  playerDash();
+    // checks if the player dashes
+    playerDash();
 
-  playerMove();
+    // calculates all player movements
+    playerMove();
 
-  lasers();
+    // creates the lasers
+    lasers();
+  }
 }
